cdb46697ee81fa7e07e4b08a6df29f45f09801b6
Karen Xie <karen.xie@myob.com>
2017-10-17 09:19:49 +1300

    Extracted out mastermindGame fields

--- MastermindSwap/MastermindGame.cs
+++ MastermindSwap/MastermindGame.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,22 +10,36 @@ namespace MastermindSwap
 {
     class MastermindGame
     {
+        private string _code;
+        private string _mark;
+        private Mastermind _mastermind;
+        private InputCleanser _inputCleanser;
+
+
+        public MastermindGame()
+        {
+            _mastermind = new Mastermind(new GuessValidator());
+            _inputCleanser = new InputCleanser();
+            _mark = "";
+        }
         public void StartGame()
         {
-            var mastermind = new Mastermind(new GuessValidator());
-            var inputCleanser = new InputCleanser();
-            var mark = "";
-            const string code = "r,g,y,c";
+            _code = CreateCode();
+
+
+
+
+            
 
             Console.WriteLine("Enter in your guess (4 characters, r g y c w)");
-            while (!mastermind.CheckWinCondition(mark))
+            while (!_mastermind.CheckWinCondition(_mark))
             {
                 try
                 {
                     var input = Console.ReadLine();
-                    var guess = inputCleanser.AddCommas(input);
-                    mark = mastermind.ReturnMark(code, guess);
-                    Console.WriteLine(mark.Equals("") ? "----" : mark);
+                    var guess = _inputCleanser.AddCommas(input);
+                    _mark = _mastermind.ReturnMark(_code, guess);
+                    Console.WriteLine(_mark.Equals("") ? "----" : _mark);
                 }
                 catch (ArgumentException e)
                 {
@@ -39,6 +54,9 @@ namespace MastermindSwap
             Console.WriteLine("You win! :-)");
         }
 
-
+        private string CreateCode()
+        {
+             return "r,g,y,c";
+        }
     }
 }
