43c8a9908dc6c7eb0fa85d3b98c98618e24ba6ed
Karen Xie <karen.xie@myob.com>
2017-10-16 16:59:44 +1300

    first commit

---
+++ MastermindSwap/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MastermindSwap
+{
+    public class GameState
+    {
+        public bool hasFinished { get; set; }
+        public bool hasStarted { get; set; }
+    }
+}
--- MastermindSwap/Program.cs
+++ MastermindSwap/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using MastermindKata.Logic;
 
 namespace MastermindSwap
 {
@@ -10,6 +12,12 @@ namespace MastermindSwap
     {
         static void Main(string[] args)
         {
+            MastermindKata.Logic.Mastermind mastermind = new Mastermind(new GuessValidator());
+            string secret = "r,g,y,c";
+            string guess = "r,g,y,c"; // Can only guess using rgycw
+            string mark = mastermind.ReturnMark(secret, guess);
+
+            Console.WriteLine(secret + " " + guess + " " + mark);
         }
     }
 }
